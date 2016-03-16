3380bde507c752b2bb0ff0104ff30f7d92d957a4
Ionov George <g.ionov@php73.ru>
2016-03-16 18:27:00 +0300

    Validators tests

--- Tests/IntegerValidatorTest.php
+++ Tests/IntegerValidatorTest.php
@@ -11,7 +11,7 @@ class IntegerValidatorTest extends PHPUnit_Framework_TestCase
 {
     public function testMin(){
         $validator = new IntegerValidator();
-        $validator->min = 0;
+        $validator->setMin(0);
 
         $this->assertFalse($validator->isValid('-1'));
         $this->assertFalse($validator->isValid(''));
@@ -27,6 +27,39 @@ class IntegerValidatorTest extends PHPUnit_Framework_TestCase
     }
 
     public function testMax(){
+        $validator = new IntegerValidator();
+        $validator->setMax(100);
 
+        $this->assertFalse($validator->isValid(''));
+        $this->assertFalse($validator->isValid('dfsdfsdf'));
+        $this->assertFalse($validator->isValid(null));
+        $this->assertFalse($validator->isValid([]));
+        $this->assertFalse($validator->isValid('12312.231'));
+        $this->assertFalse($validator->isValid(12312.231));
+        $this->assertFalse($validator->isValid(12312.231));
+        $this->assertFalse($validator->isValid(1432));
+
+        $this->assertTrue($validator->isValid(1));
+        $this->assertTrue($validator->isValid('-1'));
+    }
+
+    public function testMinMax(){
+        $validator = new IntegerValidator();
+        $validator->setMin(0);
+        $validator->setMax(100);
+
+        $this->assertFalse($validator->isValid(''));
+        $this->assertFalse($validator->isValid('dfsdfsdf'));
+        $this->assertFalse($validator->isValid(null));
+        $this->assertFalse($validator->isValid([]));
+        $this->assertFalse($validator->isValid('12312.231'));
+        $this->assertFalse($validator->isValid(12312.231));
+        $this->assertFalse($validator->isValid(12312.231));
+        $this->assertFalse($validator->isValid(1432));
+        $this->assertFalse($validator->isValid('101'));
+        $this->assertFalse($validator->isValid('-1'));
+
+        $this->assertTrue($validator->isValid(1));
+        $this->assertTrue($validator->isValid('100'));
     }
 }
--- Tests/StringValidatorTest.php
+++ Tests/StringValidatorTest.php
@@ -12,7 +12,7 @@ class StringValidatorTest extends PHPUnit_Framework_TestCase
     public function testLength()
     {
         $validator = new StringValidator();
-        $validator->length(5);
+        $validator->setLength(5);
 
         $this->assertFalse($validator->isValid('dasdas'));
         $this->assertFalse($validator->isValid('dasd'));
@@ -26,7 +26,7 @@ class StringValidatorTest extends PHPUnit_Framework_TestCase
     public function testMinLength()
     {
         $validator = new StringValidator();
-        $validator->minLength(5);
+        $validator->setMinLength(5);
 
         $this->assertFalse($validator->isValid('dasd'));
         $this->assertFalse($validator->isValid(null));
@@ -40,7 +40,7 @@ class StringValidatorTest extends PHPUnit_Framework_TestCase
     public function testMaxLength()
     {
         $validator = new StringValidator();
-        $validator->maxLength(5);
+        $validator->setMaxLength(5);
 
         $this->assertFalse($validator->isValid(null));
         $this->assertFalse($validator->isValid(['dfsdf']));
@@ -54,8 +54,8 @@ class StringValidatorTest extends PHPUnit_Framework_TestCase
     public function testMinMaxLength()
     {
         $validator = new StringValidator();
-        $validator->minLength(5);
-        $validator->maxLength(10);
+        $validator->setMinLength(5);
+        $validator->setMaxLength(10);
 
         $this->assertFalse($validator->isValid('dasd'));
         $this->assertFalse($validator->isValid(null));
@@ -70,7 +70,7 @@ class StringValidatorTest extends PHPUnit_Framework_TestCase
     public function testRegexp1()
     {
         $validator = new StringValidator();
-        $validator->regexp('/^1{10}$/u');
+        $validator->setRegexp('/^1{10}$/u');
 
         $this->assertFalse($validator->isValid('dasd'));
         $this->assertFalse($validator->isValid(null));
@@ -85,7 +85,7 @@ class StringValidatorTest extends PHPUnit_Framework_TestCase
     public function testRegexp2()
     {
         $validator = new StringValidator();
-        $validator->regexp('/^1*$/u');
+        $validator->setRegexp('/^1*$/u');
 
         $this->assertFalse($validator->isValid('dasd'));
         $this->assertFalse($validator->isValid(null));
